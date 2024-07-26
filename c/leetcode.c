e63e359ea3c9bcbec8146b848114b014cdfff610
dylanR33 <ruiz.dylaneduardo@gmail.com>
2024-07-25 17:56:54 -0700

    Completed Is Subsequence problem.

--- c/leetcode.c
+++ c/leetcode.c
@@ -1,7 +1,28 @@
 #include "leetcode.h"
 
-void rotate_linear_space_helper(int* nums, int numsSize, int k, 
-                                int left_element, int right_element) {
+int isSubsequence(char* s, char* t) {
+    char* s_ptr = s;
+    char* t_ptr = t;
+
+    while (*s_ptr) {
+        // We reached the end of t before s: not a subsequence.
+        if (!*t_ptr && *s_ptr) {
+            return 0;
+        // The char is s matches char in t, so advance.
+        } else if (*s_ptr == *t_ptr) {
+            s_ptr++;
+            t_ptr++;
+        // No match, continue across t searching for a match.
+        } else {
+            t_ptr++;
+        }
+    }
+    return 1;
+}
+
+
+void rotate_linear_space_helper(int* nums, int numsSize, int left_element, 
+                                int right_element) {
     int temp = 0;
     // Reverse the elements of an array.
     while (left_element < right_element) {
@@ -13,6 +34,7 @@ void rotate_linear_space_helper(int* nums, int numsSize, int k,
     }
 }
 
+
 void rotate_linear_space(int* nums, int numsSize, int k) {
     // Ensure that k is within the range of the array. If it is 
     // greater than or equal to the size of the array then the remainder 
@@ -22,18 +44,19 @@ void rotate_linear_space(int* nums, int numsSize, int k) {
     // Reverse the full array.
     int left_element = 0;
     int right_element = numsSize - 1;
-    rotate_linear_space_helper(nums, numsSize, k, left_element, right_element);
+    rotate_linear_space_helper(nums, numsSize, left_element, right_element);
 
     // Reverse up to the k - 1 element
     left_element = 0;
     right_element = k - 1;
-    rotate_linear_space_helper(nums, numsSize, k, left_element, right_element);
+    rotate_linear_space_helper(nums, numsSize, left_element, right_element);
 
     // Reverse the remaining portion (from the kth element to the size-1 element)
     left_element = k;
     right_element = numsSize - 1;
-    rotate_linear_space_helper(nums, numsSize, k, left_element, right_element);
+    rotate_linear_space_helper(nums, numsSize, left_element, right_element);
 }
+
 
 void rotate_o_of_n_space(int* nums, int numsSize, int k) {
     // Create a new array the same size as the one being passed in.
--- c/leetcode.h
+++ c/leetcode.h
@@ -4,11 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void rotate_linear_space_helper(int* nums, int numsSize, int k, 
-                                int left_element, int right_element);
+// Determine if the string s is a subsequence of the string t.
+//
+// @param s : subsequence string.
+//
+// @param t : main string.
+int isSubsequence(char* s, char* t);
 
+
+// Reverse array from the bounds of left_element to right_element.
+//
+// @param nums : array to reverse.
+//
+// @param numsSize : number of elements in nums.
+//
+// @param left_element : left bound of reversing.
+//
+// @param right_element : right bound of reversing.
+void rotate_linear_space_helper(int* nums, int numsSize, int left_element, 
+                                int right_element);
+
+
+// Rotate an array by k number of steps.
+//
+// @param nums : array to rotate.
+//
+// @param numsSize : number of elements in nums.
+//
+// @param k : number of steps to rotate.
 void rotate_linear_space(int* nums, int numsSize, int k);
-
 void rotate_o_of_n_space(int* nums, int numsSize, int k);
+
 
 #endif
